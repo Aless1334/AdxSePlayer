c12239ece0f8f2efcd7efd075d910156709206eb
Aless1334 <aless3511@gmail.com>
2019-06-14 00:27:32 +0900

    キューシート選択実装

--- Support/Editor/SelectCueInfoSupportEditor.cs
+++ Support/Editor/SelectCueInfoSupportEditor.cs
@@ -7,19 +7,31 @@ namespace AdxSePlayer.Support.Editor
     [CustomEditor(typeof(SelectCueInfoSupport))]
     public class SelectCueInfoSupportEditor : UnityEditor.Editor
     {
+        private CriAtom _atomObject;
+
         public override void OnInspectorGUI()
         {
             var selectCue = target as SelectCueInfoSupport;
             if (!selectCue) return;
 
-            selectCue.selectedSheetIndex = EditorGUILayout.Popup(selectCue.selectedSheetIndex, new string[] { });
+            if (!_atomObject)
+            {
+                _atomObject = FindObjectOfType<CriAtom>();
+                if (!_atomObject)
+                {
+                    EditorGUILayout.LabelField("Please make CriAtom in Hierarchy.");
+                    return;
+                }
+            }
 
-            var names = typeof(CriAtom).GetProperty("instance",
-                BindingFlags.Static | BindingFlags.NonPublic);
+            var _cueSheetNames = new string[_atomObject.cueSheets.Length];
+            for (var i = 0; i < _cueSheetNames.Length; i++)
+                _cueSheetNames[i] = _atomObject.cueSheets[i].name;
 
+            selectCue.selectedSheetIndex =
+                EditorGUILayout.Popup("Cue Sheet", selectCue.selectedSheetIndex, _cueSheetNames);
 
-
-            Debug.Log(names.GetValue("cueSheets", BindingFlags.NonPublic));
+            EditorUtility.SetDirty(selectCue);
         }
     }
 }
