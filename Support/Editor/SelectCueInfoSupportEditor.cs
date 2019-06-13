5648572bd3fd260dd23d8c9bb085817976cd854f
Aless1334 <aless3511@gmail.com>
2019-06-13 22:58:02 +0900

    共有

---
+++ Support/Editor/SelectCueInfoSupportEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace AdxSePlayer.Support.Editor
+{
+    [CustomEditor(typeof(SelectCueInfoSupport))]
+    public class SelectCueInfoSupportEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            var selectCue = target as SelectCueInfoSupport;
+            if (!selectCue) return;
+
+            selectCue.selectedSheetIndex = EditorGUILayout.Popup(selectCue.selectedSheetIndex, new string[] { });
+
+            var names = typeof(CriAtom).GetProperty("instance",
+                BindingFlags.Static | BindingFlags.NonPublic);
+
+
+
+            Debug.Log(names.GetValue("cueSheets", BindingFlags.NonPublic));
+        }
+    }
+}
